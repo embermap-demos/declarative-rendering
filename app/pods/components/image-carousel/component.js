706be79a9cc42af64957faee68e2fc632bcd07af
Sam Selikoff <sam.selikoff@gmail.com>
2018-03-29 16:42:14 -0400

    Simpler start

--- app/pods/components/image-carousel/component.js
+++ app/pods/components/image-carousel/component.js
@@ -9,7 +9,7 @@ export default Component.extend({
     moveRight() {
       let container = this.$('.carousel-viewport');
       // Get the current transform percentage
-      let current = +(container.css('transform').replace(/[^0-9\-.,]/g, '').split(',')[4]) / 1280 * 100;
+      let current = +(container.css('transform').replace(/[^0-9\-.,]/g, '').split(',')[4]) / 1280 * 100 || 0;
       // Add the slide (width + margin)
       let shifted = current - 62.5;
 
@@ -18,7 +18,7 @@ export default Component.extend({
 
     moveLeft() {
       let container = this.$('.carousel-viewport');
-      let current = +container.css('transform').replace(/[^0-9\-.,]/g, '').split(',')[4] / 1280 * 100;
+      let current = +container.css('transform').replace(/[^0-9\-.,]/g, '').split(',')[4] / 1280 * 100 || 0;
       let shifted = current + 62.5;
 
       container.css('transform', `translate(${shifted}%)`);
